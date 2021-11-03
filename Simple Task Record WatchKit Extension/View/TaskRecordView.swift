a6ae2328b37cafc03d2faa607e03c100a6f002f0
toshiki.nagahama <exevolute@gmail.com>
2021-11-03 16:24:05 +0900

    タスク時間記録画面作成。

--- Simple Task Record WatchKit Extension/View/TaskRecordView.swift
+++ Simple Task Record WatchKit Extension/View/TaskRecordView.swift
@@ -11,37 +11,59 @@ import SwiftUI
 struct TaskRecordView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     @EnvironmentObject var vm: ViewModel
-    let taskName: String
+    @State var showingSaveResultAlert = false //保存時アラート表示フラグ
+    @State var alertTitle = "" //保存時アラートタイトル
+    @State var alertMessage = "" //保存時アラートメッセージ
+    let taskName: String //アクティブタスク
     
     var body: some View {
         VStack(){
-            HStack(){
-                Text(String(format: "%02d:%02d", (vm.totalElapsedTime_last + vm.elapsedTime) / 60, (vm.totalElapsedTime_last + vm.elapsedTime) % 60))
-                    .padding()
-                Button(action: {
-                    vm.stop()
-                    vm.totalElapsedTime_last += vm.elapsedTime
-                    vm.elapsedTime = 0
-                    print(vm.totalElapsedTime_last)
-                }){
-                    Text("一時停止")
-                        .padding()
+            Group{
+                if vm.state == 0{
+                    Text("not recording")
+                        .foregroundColor(.gray)
+                }else if vm.state == 1{
+                    Text("recording")
+                        .foregroundColor(.red)
+                }else if vm.state == 2{
+                    Text("pause")
+                        .foregroundColor(.yellow)
                 }
             }
             HStack(){
-                Button(action: {
-                    vm.activeTaskName = taskName
-                    vm.start()
-                }){
-                    Text("開始")
-                        .padding()
+                Text(String(format: "%02d:%02d", (vm.totalElapsedTime_last + vm.elapsedTime) / 60, (vm.totalElapsedTime_last + vm.elapsedTime) % 60))
+                    .padding()
+                if vm.state == 1{
+                    Button(action: {
+                        vm.pause()
+                    }){
+                        Text("一時停止")
+                            .padding()
+                    }
+                }else if vm.state == 2{
+                    Button(action: {
+                        vm.resume()
+                    }){
+                        Text("再開")
+                            .padding()
+                    }
                 }
+                
+            }
+            if vm.state == 0{
                 Button(action: {
-                    vm.activeTaskName = ""
-                    vm.endTime = Date()
+                    vm.pause()
+                }){
+                    Button(action: {
+                        vm.start(_taskName: taskName)
+                    }){
+                        Text("開始")
+                            .padding()
+                    }
+                }
+            }else{
+                Button(action: {
                     vm.stop()
-                    vm.totalElapsedTime = vm.totalElapsedTime_last + vm.elapsedTime
-                    vm.totalElapsedTime_last = 0
                     let record = MyTaskRecord(context: managedObjectContext)
                     record.endTime = vm.endTime
                     record.startTime = vm.startTime
@@ -50,8 +72,14 @@ struct TaskRecordView: View {
                     do {
                         try managedObjectContext.save()
                         print("success to save record")
+                        self.alertTitle = "success to save"
+                        self.alertMessage = String(format: "Time is %02d:%02d", (vm.totalElapsedTime) / 60, (vm.totalElapsedTime) % 60)
+                        self.showingSaveResultAlert = true
                     } catch {
                         print(error)
+                        self.alertTitle = "fail to save"
+                        self.alertMessage = ""
+                        self.showingSaveResultAlert = true
                         print("fail to save record")
                     }
                     
@@ -60,7 +88,13 @@ struct TaskRecordView: View {
                         .padding()
                 }
             }
-        }
+       }
+      .alert(isPresented: $showingSaveResultAlert) {
+           Alert(
+               title: Text(alertTitle),
+               message: Text(alertMessage)
+           )
+       }
         .navigationBarTitle(Text(taskName))
         
     }
